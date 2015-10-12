fc92c7fcdc5afd10cc0f2196a86a9aba2ac1b172
Jontae Pierce <jontae.pierce@gmail.com>
2015-10-12 16:49:56 -0400

    pushed appending function inside the ajax function to post api data

--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
+var itemstuff = '';
 (function () {
 
-  console.log('It Works!');
+let etsyURL = 'https://api.etsy.com/v2/listings/active.js?api_key=h9oq2yf3twf4ziejn10b717i&keywords=computer+chair&includes=Images,Shop';
+var templateString = $('#itemTemplate').text();
+var templateFunction = _.template(templateString);
 
-}());
+  $.ajax({
+      url: etsyURL,
+      dataType: 'jsonp',
+      method: 'get'
+    }).then (function (response) {
+      // console.log(response);
+      // itemstuff = response;
+    _.each(response.results, function (item) {
+      var itemHTML = templateFunction(item);
+      $('.container').append(itemHTML);
+    });
+  });
+
+
+  })();
