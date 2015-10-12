6435f6beb907807c1d699560e686aa4385b539d8
Jontae Pierce <jontae.pierce@gmail.com>
2015-10-12 15:55:21 -0400

    Initial Commit

---
+++ app/js/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+(function () {
+
+var templateString = $('#itemTemplate').text();
+var templateFunction = _.template(templateString);
+
+_.each(etsy.results, function (item) {
+  var itemHTML = templateFunction(item);
+  $('.container').append(itemHTML);
+});
+
+
+}());
